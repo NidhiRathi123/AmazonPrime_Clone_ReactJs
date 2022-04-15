40bb9376367b850f1ecc3adca9d2e9f3caaf75d4
Nidhi Rathi <nidhirathi2312@gmail.com>
2022-04-15 19:12:54 +0530

    Updates

---
+++ precache-manifest.8273fd46db991aa67a13bb546a026eea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1db918893101405fd2eccd01148406e5",
+    "url": "/AmazonPrime_Clone_ReactJs/index.html"
+  },
+  {
+    "revision": "c5a614f6f4b014009f2a",
+    "url": "/AmazonPrime_Clone_ReactJs/static/css/main.f6ac3ec9.chunk.css"
+  },
+  {
+    "revision": "92ec4e2a8b0dbed34c74",
+    "url": "/AmazonPrime_Clone_ReactJs/static/js/2.2b079a66.chunk.js"
+  },
+  {
+    "revision": "c0ec89e059f7c30077b7700139acef52",
+    "url": "/AmazonPrime_Clone_ReactJs/static/js/2.2b079a66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5a614f6f4b014009f2a",
+    "url": "/AmazonPrime_Clone_ReactJs/static/js/main.01de79cd.chunk.js"
+  },
+  {
+    "revision": "475f4090ac3fbfbbdbbf",
+    "url": "/AmazonPrime_Clone_ReactJs/static/js/runtime-main.cb05f7b1.js"
+  }
+]);
